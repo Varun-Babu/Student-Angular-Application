0a450839cae363a4960a82cb31b4fd327a2920bf
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-24 14:50:17 +0530

    view students added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { ViewStudentsComponent } from './view-students/view-students.component';
 
+const myRoute: Routes=[
+  {
+    path:"",
+    component:StudentEntryComponent
+  },
+  {
+    path:"view",
+    component:ViewStudentsComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    StudentEntryComponent
+    StudentEntryComponent,
+    ViewStudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
